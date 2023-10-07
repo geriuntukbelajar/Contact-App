048a8ea86129a1c7cf07aa77fd5c27b16abc9b84
geriard16 <geriard16charitas@gmail.com>
2023-10-08 02:33:32 +0700

    menggunakan metode promise pada app

--- app.js
+++ app.js
@@ -18,19 +18,29 @@ if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-rl.question('Masukkan nama anda: ', (nama) => {
-    rl.question('Masukkan No Hp anda: ', (noHp) => {
-        const contact = {
-            nama,
-            noHp,
-        };
-        const file = fs.readFileSync('data/contacts.json','utf-8');
-        const contacts = JSON.parse(file);
-
-        contacts.push(contact);
-
-        fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
-        console.log('Terima kasih sudah memasukkan data anda');
-        rl.close();
+const tulisPertanyaan = (pertanyaan) => {
+    return new Promise((resolve, reject) => {
+        rl.question(pertanyaan, (jawaban) => {
+            resolve(jawaban);
+        });
     });
-});
+};
+
+
+const main = async () => {
+    const nama = await tulisPertanyaan('Masukkan nama anda: ');
+    const email = await tulisPertanyaan('Masukkan email anda: ');
+    const noHp = await tulisPertanyaan('Masukkan no Hp anda: ');
+    
+    const contact = {nama, email, noHp};
+    const file = fs.readFileSync('data/contacts.json','utf-8');
+    const contacts = JSON.parse(file);
+
+    contacts.push(contact);
+
+    fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
+    console.log('Terima kasih sudah memasukkan data anda');
+    rl.close();
+}
+
+main();
