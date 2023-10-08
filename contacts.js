d0d985796714fd56070c3e25c6f759b01158e0ef
geriuntukbelajar <geripbw@gmail.com>
2023-10-08 23:33:15 +0700

    menambahkan validasi nama sudah terdaftar pada file contacts

--- contacts.js
+++ contacts.js
@@ -1,10 +1,4 @@
 const fs = require('node:fs');
-const readline = require('node:readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
 
 // membuat folder data kalau gak ada
 const dirPath = './data';
@@ -18,24 +12,23 @@ if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (jawaban) => {
-            resolve(jawaban);
-        });
-    });
-};
-
 const simpanContact = (nama,email,noHp) => {
     const contact = {nama, email, noHp};
     const file = fs.readFileSync('data/contacts.json','utf-8');
     const contacts = JSON.parse(file);
 
+    //cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama);
+    if(duplikat){
+        console.log('Contact sudah terdaftar, gunakan nama lain!');
+        return false;
+    }
+
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
     console.log('Terima kasih sudah memasukkan data anda');
-    rl.close();
+
 };
 
-module.exports = {tulisPertanyaan, simpanContact};
+module.exports = {simpanContact};
